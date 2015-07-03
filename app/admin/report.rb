aba580c9150ce95155ea0eeda2b6e75dc43a4427
gioch <georgechkhvirkia@gmail.com>
2015-07-04 02:38:48 +0400

    add reports for past 4 weeks

--- app/admin/report.rb
+++ app/admin/report.rb
@@ -2,27 +2,25 @@ ActiveAdmin.register_page "Report" do
 
   content title: "Reports" do
 
+    ## made calculation only for past 4 weeks, 
+    ## but functionality is ready to get by any given time range
+
     current_date = Date.today.strftime("%Y-%m-%d")
-    week_ago = 7.days.ago.strftime("%Y-%m-%d")
+    weeks_ago = 4.weeks.ago.strftime("%Y-%m-%d")
 
-    apple_revenue_week_ago = Apple.revenue_between(week_ago, current_date)
-    stripe_revenue_week_ago = Stripe.revenue_between(week_ago, current_date)
+    apple_revenue = Apple.revenue_between(weeks_ago, current_date)
+    stripe_revenue = Stripe.revenue_between(weeks_ago, current_date)
 
-    total_revenue_week_ago = 0
-    total_revenue_week_ago += apple_revenue_week_ago if !apple_revenue_week_ago.nil?
-    total_revenue_week_ago += stripe_revenue_week_ago if !stripe_revenue_week_ago.nil?
-
-    four_week_ago = 4.weeks.ago.strftime("%Y-%m-%d")
-
-    apple_revenue_four_week_ago = Apple.revenue_between(four_week_ago, current_date)
-    stripe_revenue_four_week_ago = Stripe.revenue_between(four_week_ago, current_date)
-
+    total_revenue = 0
+    total_revenue += apple_revenue if !apple_revenue.nil?
+    total_revenue += stripe_revenue if !stripe_revenue.nil?
 
     render partial: 'report', locals: { 
-      date: week_ago, 
-      apple_revenue_week_ago: apple_revenue_week_ago, 
-      stripe_revenue_week_ago: stripe_revenue_week_ago, 
-      total_revenue_week_ago: total_revenue_week_ago,
+      current_date: current_date, 
+      weeks_ago: weeks_ago, 
+      apple_revenue: apple_revenue, 
+      stripe_revenue: stripe_revenue, 
+      total_revenue: total_revenue,
     }
   end # content
 
--- app/models/premium_plan.rb
+++ app/models/premium_plan.rb
@@ -4,17 +4,19 @@ class PremiumPlan < ActiveRecord::Base
 
   @@additional_fee = 0
 
+  ## method shared by every subclass
   def monthly_charge
     self.price_per_month - ( self.price_per_month * @@additional_fee / 100 )
   end
 
+  ## get subscriptions of current premium plane in give date range
   def self.subscriptions_between(start_date, end_date)
     self.first.subscriptions.between_dates(start_date, end_date)
   end
 
+  ## calculate total revenue for given plan in given date range
   def self.revenue_between(start_date, end_date)
     subs = self.subscriptions_between(start_date, end_date)
-
     subs.map{|sbs| sbs.premium_plan.monthly_charge }.inject(:+) unless subs.nil?
   end
 end
